42ad9880948d0ce4e5db543f0bb628ffd4991e1e
RyanDeBarros <ryan2004db@gmail.com>
2023-11-22 03:43:11 -0500

    Created TileElement class.

--- MapCreator/app/src/main/java/main/App.java
+++ MapCreator/app/src/main/java/main/App.java
@@ -1,9 +1,19 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 public class App extends Application {
+
+	private final ArrayList<TileElement> tileElements = new ArrayList<>();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -12,6 +22,35 @@ public class App extends Application {
 	@Override
 	public void start(Stage stage) {
 
+	}
+
+	private static Pane process(ArrayList<TileElement> tileElements) {
+		Pane pane = new Pane();
+		for (TileElement tileElement : tileElements) {
+			ImageView imgV = new ImageView(tileElement.image);
+			imgV.setLayoutX(tileElement.x * TileElement.WIDTH);
+			imgV.setLayoutY(tileElement.y * TileElement.HEIGHT);
+			pane.getChildren().add(imgV);
+//			if (pane.getPrefWidth() < imgV.getLayoutX() + TileElement.WIDTH) {
+//				pane.setPrefWidth(imgV.getLayoutX() + TileElement.WIDTH);
+//			}
+//			if (pane.getPrefHeight() < imgV.getLayoutY() + TileElement.HEIGHT) {
+//				pane.setPrefHeight(imgV.getLayoutY() + TileElement.HEIGHT);
+//			}
+		}
+		return pane;
+	}
+
+	private static void snapshot(Pane previewPane, File file) {
+//		SnapshotParameters snapshot = new SnapshotParameters();
+//		snapshot.setViewport(new javafx.geometry.Rectangle2D(0, 0, previewPane.getPrefWidth(), previewPane.getPrefHeight()));
+//		WritableImage img = previewPane.snapshot(snapshot, null);
+		WritableImage img = previewPane.snapshot(null, null);
+		try {
+			ImageIO.write(SwingFXUtils.fromFXImage(img, null), "png", file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ MapCreator/app/src/main/java/main/TileElement.java
@@ -0,0 +1,11 @@
+package main;
+
+import javafx.scene.image.Image;
+
+public class TileElement {
+
+	public Image image;
+	public int x, y;
+	public static final int WIDTH = 64, HEIGHT = 64;
+
+}
