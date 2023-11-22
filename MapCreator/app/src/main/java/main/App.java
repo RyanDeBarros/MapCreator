d4b1bd8c75a395fd3f0512b7da8fc55c4eab46ae
RyanDeBarros <ryan2004db@gmail.com>
2023-11-22 06:24:22 -0500

    Started UI design.

--- MapCreator/app/src/main/java/main/App.java
+++ MapCreator/app/src/main/java/main/App.java
@@ -5,15 +5,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 
 public class App extends Application {
 
 	private final ArrayList<TileElement> tileElements = new ArrayList<>();
+	private final Spinner spinX = new Spinner<Integer>(0, Integer.MAX_VALUE, 0),
+			spinY = new Spinner<Integer>(0, Integer.MAX_VALUE, 0);
+	private final Button open = new Button();
+	private final FileChooser openImage = new FileChooser();
+	private File openAt;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -21,22 +33,58 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) {
+		DirectoryChooser onStart = new DirectoryChooser();
+		openAt = onStart.showDialog(stage);
+
+		Pane menu = new Pane();
+		menu.setPrefSize(1200, 400);
+		spinX.setLayoutX(20);
+		spinX.setLayoutY(20);
+		spinX.setEditable(true);
+		spinX.setPrefWidth(80);
+		spinY.setLayoutX(120);
+		spinY.setLayoutY(20);
+		spinY.setEditable(true);
+		spinY.setPrefWidth(80);
+		open.setLayoutX(220);
+		open.setLayoutY(20);
+		open.setPrefWidth(90);
+		open.setText("Add image");
+		open.setTextAlignment(TextAlignment.CENTER);
+		open.setOnAction(a -> {
+			openImage.setInitialDirectory(openAt);
+			File imageFile = openImage.showOpenDialog(stage);
+			if (null != imageFile) {
+				openAt = imageFile.getParentFile();
+				Image image = new Image(imageFile.getAbsolutePath());
+				addImage(image, (int) spinX.getValue(), (int) spinY.getValue());
+			}
+		});
+		menu.getChildren().addAll(spinX, spinY, open);
+		stage.setScene(new Scene(menu));
+		stage.setTitle("Map Creator");
+		stage.show();
+	}
+
+	private void addImage(Image img, int x, int y) {
 
 	}
 
 	private static Pane process(ArrayList<TileElement> tileElements) {
 		Pane pane = new Pane();
 		for (TileElement tileElement : tileElements) {
-			ImageView imgV = new ImageView(tileElement.image);
-			imgV.setLayoutX(tileElement.x * TileElement.WIDTH);
-			imgV.setLayoutY(tileElement.y * TileElement.HEIGHT);
-			pane.getChildren().add(imgV);
-//			if (pane.getPrefWidth() < imgV.getLayoutX() + TileElement.WIDTH) {
-//				pane.setPrefWidth(imgV.getLayoutX() + TileElement.WIDTH);
-//			}
-//			if (pane.getPrefHeight() < imgV.getLayoutY() + TileElement.HEIGHT) {
-//				pane.setPrefHeight(imgV.getLayoutY() + TileElement.HEIGHT);
-//			}
+			for (Image image : tileElement.images) {
+				ImageView imgV = new ImageView(image);
+				imgV.setLayoutX(tileElement.x * TileElement.WIDTH);
+				imgV.setLayoutY(tileElement.y * TileElement.HEIGHT);
+				pane.getChildren().add(imgV);
+//				if (pane.getPrefWidth() < imgV.getLayoutX() + TileElement.WIDTH) {
+//					pane.setPrefWidth(imgV.getLayoutX() + TileElement.WIDTH);
+//				}
+//				if (pane.getPrefHeight() < imgV.getLayoutY() + TileElement.HEIGHT) {
+//					pane.setPrefHeight(imgV.getLayoutY() + TileElement.HEIGHT);
+//				}
+			}
 		}
 		return pane;
 	}
--- MapCreator/app/src/main/java/main/TileElement.java
+++ MapCreator/app/src/main/java/main/TileElement.java
@@ -1,10 +1,11 @@
 package main;
 
+import java.util.ArrayList;
 import javafx.scene.image.Image;
 
 public class TileElement {
 
-	public Image image;
+	public final ArrayList<Image> images = new ArrayList<>();
 	public int x, y;
 	public static final int WIDTH = 64, HEIGHT = 64;
 
