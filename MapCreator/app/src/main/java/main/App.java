8cef7a7c1423566590bfc1e4c445dcc6e1967187
RyanDeBarros <ryan2004db@gmail.com>
2023-11-22 07:27:18 -0500

    TileList class, adding images, and displaying stack.

--- MapCreator/app/src/main/java/main/App.java
+++ MapCreator/app/src/main/java/main/App.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,12 +21,13 @@ import javax.imageio.ImageIO;
 
 public class App extends Application {
 
-	private final ArrayList<TileElement> tileElements = new ArrayList<>();
+	private final TileList tileList = new TileList();
 	private final Spinner spinX = new Spinner<Integer>(0, Integer.MAX_VALUE, 0),
 			spinY = new Spinner<Integer>(0, Integer.MAX_VALUE, 0);
 	private final Button open = new Button();
 	private final FileChooser openImage = new FileChooser();
 	private File openAt;
+	private Pane displayStack = new Pane();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -37,7 +39,12 @@ public class App extends Application {
 		openAt = onStart.showDialog(stage);
 
 		Pane menu = new Pane();
-		menu.setPrefSize(1200, 400);
+		menu.setPrefSize(1200, 450);
+		displayStack.setPrefSize(1100, 300);
+		displayStack.setLayoutX(50);
+		displayStack.setLayoutY(100);
+		menu.getChildren().add(displayStack);
+
 		spinX.setLayoutX(20);
 		spinX.setLayoutY(20);
 		spinX.setEditable(true);
@@ -54,7 +61,7 @@ public class App extends Application {
 		open.setOnAction(a -> {
 			openImage.setInitialDirectory(openAt);
 			File imageFile = openImage.showOpenDialog(stage);
-			if (null != imageFile) {
+			if (null != imageFile && isPNG(imageFile)) {
 				openAt = imageFile.getParentFile();
 				Image image = new Image(imageFile.getAbsolutePath());
 				addImage(image, (int) spinX.getValue(), (int) spinY.getValue());
@@ -66,8 +73,33 @@ public class App extends Application {
 		stage.show();
 	}
 
-	private void addImage(Image img, int x, int y) {
+	private static boolean isPNG(File file) {
+		return Files.getFileExtension(file.getAbsolutePath()).equals("png");
+	}
 
+	private void addImage(Image img, int x, int y) {
+		TileElement tileElement = tileList.get(new Coo(x, y));
+		if (null == tileElement) {
+			tileElement = new TileElement();
+			tileElement.c = new Coo(x, y);
+			if (!tileList.add(tileElement)) {
+				System.err.println("TileElement " + tileElement + " already has coordinates in tileList.");
+				System.exit(1);
+			}
+		}
+		tileElement.images.add(img);
+		displayTileStack();
+	}
+
+	private void displayTileStack() {
+		displayStack.getChildren().clear();
+		TileElement tileElement = tileList.get(new Coo((int) spinX.getValue(), (int) spinY.getValue()));
+		for (int i = 0; i < tileElement.images.size(); i++) {
+			ImageView imgV = new ImageView(tileElement.images.get(i));
+			imgV.setLayoutX(i * tileElement.images.get(i).getWidth());
+			imgV.setLayoutY(0.5 * (displayStack.getPrefHeight() - tileElement.images.get(i).getHeight()));
+			displayStack.getChildren().add(imgV);
+		}
 	}
 
 	private static Pane process(ArrayList<TileElement> tileElements) {
@@ -75,8 +107,8 @@ public class App extends Application {
 		for (TileElement tileElement : tileElements) {
 			for (Image image : tileElement.images) {
 				ImageView imgV = new ImageView(image);
-				imgV.setLayoutX(tileElement.x * TileElement.WIDTH);
-				imgV.setLayoutY(tileElement.y * TileElement.HEIGHT);
+				imgV.setLayoutX(tileElement.c.x * TileElement.WIDTH);
+				imgV.setLayoutY(tileElement.c.y * TileElement.HEIGHT);
 				pane.getChildren().add(imgV);
 //				if (pane.getPrefWidth() < imgV.getLayoutX() + TileElement.WIDTH) {
 //					pane.setPrefWidth(imgV.getLayoutX() + TileElement.WIDTH);
@@ -99,6 +131,47 @@ public class App extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static class Coo {
+
+		public final int x, y;
+
+		public Coo(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			System.out.println(obj.getClass());
+			return null != obj && (obj == this || (obj instanceof Coo coo && coo.x == this.x && coo.y == this.y));
+		}
+	}
+
+	public static class TileList {
+
+		private ArrayList<Coo> coos = new ArrayList<>();
+		private ArrayList<TileElement> tileElements = new ArrayList<>();
+
+		public TileElement get(Coo coo) {
+			for (int i = 0; i < coos.size(); i++) {
+				if (coos.get(i).equals(coo)) {
+					return tileElements.get(i);
+				}
+			}
+			return null;
+		}
+
+		public boolean add(TileElement tileElement) {
+			if (null != get(tileElement.c)) {
+				return false;
+			}
+			coos.add(tileElement.c);
+			tileElements.add(tileElement);
+			return true;
+		}
+
 	}
 
 }
